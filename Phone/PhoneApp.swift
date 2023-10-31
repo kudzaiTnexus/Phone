fa8a762e681278e9daf38ac0df28523ecae593e7
Kudzai <kudzaishe@sleepiest.com>
2023-10-31 15:41:20 +0200

    first commit

---
+++ Phone/PhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneApp.swift
+//  Phone
+//
+//  Created by KudzaisheMhou on 31/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct PhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
